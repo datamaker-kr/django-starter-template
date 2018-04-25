fa36753cc97fae82d6ef664012b599fe4af61159
enoch2110 <enoch2110@me.com>
2018-04-25 17:31:47 +0900

    fixed settings

--- project_name/settings/__init__.py
+++ project_name/settings/__init__.py
@@ -1,1 +1,15 @@
-from .development import *
+try:
+    from .settings_extra import IS_PRODUCTION
+    is_production = IS_PRODUCTION
+except ImportError:
+    is_production = False
+
+if is_production:
+    from .production import *
+else:
+    from .development import *
+
+try:
+    from .settings_extra import *
+except ImportError:
+    pass
