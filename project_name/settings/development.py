ca2ca7d73aa9b574abfa3c57571ce74f7e606611
이에녹 <enoch2110@me.com>
2018-10-09 23:41:49 +0000

    settings db정보 추가 requirements 수정

--- project_name/settings/development.py
+++ project_name/settings/development.py
@@ -5,3 +5,16 @@ ALLOWED_HOSTS = ['*']
 SECRET_KEY = 'secret'
 
 DEBUG = True
+
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '{{ project_name }}',
+        'USER': 'postgres',
+        'ATOMIC_REQUESTS': True
+    }
+}
--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -5,3 +5,15 @@ ALLOWED_HOSTS = ['*']
 SECRET_KEY = 'secret'
 
 DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '{{ project_name }}',
+        'USER': 'postgres',
+        'ATOMIC_REQUESTS': True
+    }
+}
